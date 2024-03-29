fixed issue where pyaudio read would throw buffer overflow error. GOt librosa integrated in a very rough manner. Librosa still needs refinement to be actually useful

src/flask_server.py
@@ -28,6 +28,7 @@ from flask import Flask, render_template, request, jsonify
 import numpy as np
 import logging
 import warnings
+from librosa_analysis import Analyzer
 #from celery import Celery, Task
 
 # using basic template from: https://flask.palletsprojects.com/en/2.3.x/patterns/celery/
@@ -45,15 +46,17 @@ import warnings
 #    return celery_app
 
 
-# log = logging.getLogger('werkzeug')
-# log.setLevel(logging.ERROR)
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
 
 flask_app = Flask(__name__, template_folder='.')
 #flask_app.config.from_pyfile('flask_config.py')
-#celery_app = celery_init_app(flask_app)
+# celery_app = celery_init_app(flask_app)
 
 
 audio_str = ""
+librosa_data = {}
+analyzer = Analyzer()
 audio_source = ""
 audio_raw_max = 0
 AUDIO_SAVED_CHUNKS = 5
@@ -97,6 +100,7 @@ def audio_in():
                   DeprecationWarning)
     # TODO: change this later, it is just for testing
     global audio_str
+    global librosa_data
     global audio_source
     global audio_chunk
     global audio_raw_max
@@ -105,8 +109,8 @@ def audio_in():
     global AUDIO_SAVED_CHUNKS
     if request.method == 'POST':
         data = request.json
-        print(data)
         audio_chunk = np.array(data['data']).reshape(-1)
+        librosa_data = analyzer.readData(audio_chunk, 48000)
         rpeak = float(data['peak'])
         ravg = float(data['avg'])
         audio_raw_max = rpeak
@@ -130,7 +134,7 @@ def audio_in():
         #response.headers.add("Access-Control-Allow-Origin", "*")
         return response
     else:
-        response = jsonify({"bars": audio_str, "peak": audio_max_last, "source": audio_source})
+        response = jsonify({"bars": audio_str, "peak": audio_max_last, "source": audio_source, "librosa_data": librosa_data})
         # TODO: the actual CORS policy
         response.headers.add("Access-Control-Allow-Origin", "*")
         return response

src/librosa_analysis.py
@@ -1,4 +1,8 @@
+import os
+from tarfile import BLOCKSIZE
+os.environ["NUMBA_CACHE_DIR"] = "/tmp"
 import librosa
+from librosa.beat import plp
 import numpy as np
 
 class Analyzer:
@@ -6,14 +10,16 @@ class Analyzer:
         self.audioChunks = np.zeros((0, numChannels))
         self.lengths = [] #contains lengths of last eight blocks
     
-    def readData(self, chunk, samplerate):
+    def readData(self, chunk, samplerate, chunkSize = 1024):
         # print("##############################\nLIBROSA\n##################################")
         #append data to the collected chunks
         self.audioChunks = np.append(self.audioChunks, chunk)
+        print(len(self.audioChunks))
+        print(len(self.lengths))
         self.lengths.append(chunk.shape[0])
 
         #remove oldest audio chunk data. We keep only eight newest chunks
-        if (len(self.lengths) > 8):
+        if (len(self.lengths) > 4):
             self.audioChunks = self.audioChunks[self.lengths.pop(0):]
         
         #feed data into librosa and get only the data pertaining to new portion

src/macOS_pyaudio.py
@@ -40,7 +40,7 @@ print("* Recording")
 try:
     while True:
         # Read audio data from the stream
-        raw_data = stream.read(CHUNK)
+        raw_data = stream.read(CHUNK, exception_on_overflow=False)
         
         # Convert binary data to numpy array
         data = np.frombuffer(raw_data, dtype=np.int16)