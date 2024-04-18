d65185bc1ad586fc62c43585b6b04703bcbadeb4
Blake <blakecaven@outlook.com>
2024-04-17 23:23:41 -0400

    add support for sending output from misc sources to the server

--- local_application/local_misc_client.py
+++ local_application/local_misc_client.py
@@ -5,22 +5,61 @@ local audio client that runs a given command and sends the output to the server
 please submit bugs to https://github.com/BCaven/creativity-optional/issues
 
 this is supposed to be a background task started with cron/equivalent
-settings should be changed either through the command line arguments or through
-the creativity-optional frontend
+different options/inputs should be handled through an input file or command line arguments
+this should not care about server responses (except for server not responding)
 
 Tasks:
-[TODO] support for commands passed as command line arguments
-[TODO] multiple commands running at different times/refresh rates
-[TODO] remove all the audio functions (the base for this was `local_audio_client.py`)
+[DONE] support for commands passed as command line arguments
+[DONE] multiple commands running at different times/refresh rates
+[DONE] remove all the audio functions (the base for this was `local_audio_client.py`)
+[TODO] support for reading commands from a file
+[TODO] support for reading commands from stdin
+[TODO] support for different output types (int, str, float, etc)
+[TODO] multicast server IP detection
+[TODO] make sure we are asking to keep the connection alive
 
 
+Passing in commands:
+Reading commands from a file
+One command per line
+[KEY] [SLEEP_PERIOD] [COMMAND]
+
+Passing commands as arguments
+-c [KEY] [SLEEP_PERIOD] [COMMAND] --close
+breaks when an argument is '--close'
+
+Passing commands via stdin
+[KEY] [SLEEP_PERIOD] [COMMAND]
+
+Handling different types:
+Ideally we would be able to tell the server the type of the incoming data and have it process accordingly.
+We can go one of two routes with this:
+    Route 1: the server expects integer values between 0 and 100 and throws an error at anything else
+        In this route it is the user's job to write a script that converts their output to a int range (0-100)
+    
+    Route 2: server comes prebuilt with several methods of interpreting values and the user can pick which one to use
+        Now it is our job to write conversions for "often used" types (int, str, float, list)
+    
+    Route 1 is easier and I feel like it will end up being the default anyways when adding custom commands
+    Going with route 1 for now
 """
+import asyncio
 import sys
 import requests
+import subprocess
 import logging
+import shlex
+assert sys.version_info >= (3, 5)
+
 logging.basicConfig(format='[%(levelname)s] %(message)s')
 logging.getLogger().setLevel(logging.DEBUG)
 
+running = True
+session = requests.Session()
+session.verify = True
+# TODO: quality of life: multicast server detection
+# http://0.0.0.0:8000/general_in
+DOCKER_IP="http://127.0.0.1:8000/"
 
 
 def usage(return_val: int):
@@ -30,24 +69,96 @@ please write this...
 """)
     sys.exit(return_val)
 
+def process_command(command) -> str:
+    """
+    Run the given command and return the output
 
-def main():
+    The program will split the command for you if necessary
+
+    TODO: shlex!
+    TODO: test on windows
+    """
+    assert type(command) in [list, str], f"invalid command type {type(command)} only str and list are allowed"
+    logging.debug(f"command before processing: {command}")
+    processed_command = command
+    if type(command) == str:
+        processed_command = shlex.split(command) 
+    logging.debug(f"command after processing: {processed_command}")
+    result = subprocess.run(processed_command, stdout=subprocess.PIPE)
+    return result.stdout.decode('utf-8')
+
+
+async def command_thread(key, sleep_period, command,  command_type) -> bool:
+    """
+    Thread that processes a given command.
+
+    Calls the command, sends it to the server, and sleeps for the specified time
+
+    Returns a boolean - true if the process was killed normally, false if the process ended
+    prematurely (e.g. if the server did not respond)
+    """
+    global running
+    while running:
+        result = process_command(command)
+        jresult = {
+            key: result,
+            'type': command_type
+        }
+        try:
+            response = session.post(DOCKER_IP + 'general_in', json=jresult)
+        except requests.exceptions.ConnectionError:
+            logging.warning("The server did not respond, exiting...")
+            running = False
+            return False
+        await asyncio.sleep(sleep_period)
+    return True
+
+
+
+async def main():
     """
     main function, wrangles commands and sends their outputs
     """
-    DOCKER_IP="http://127.0.0.1:8000/"
+    global DOCKER_IP
     args = sys.argv[1:]
+    commands = dict()
+    reading_command = False
+    current_key = ""
+    current_command = []
+    current_sleep_period = 0
     while args:
         next = args.pop(0)
-        if next == '-ip':
+        if reading_command:
+            if next == '--close':
+                reading_command = False
+                commands[current_key] = (current_sleep_period, 'int', current_command)
+            else:
+                current_command.append(next)
+        elif next == '-ip':
             try:
                 DOCKER_IP = args.pop(0)
             except:
                 logging.error("failed to parse command line arguments -ip used but no ip given")
                 usage(1)
+        elif next == '-c':
+            try:
+                current_key = args.pop(0)
+                current_sleep_period = int(args.pop(0))
+                current_command = []
+                reading_command = True
+            except:
+                usage(1)
         else:
             usage(1)
+    async with asyncio.TaskGroup() as tg:
+        tasks = []
+        for key in commands:
+            sleep_period, output_type, command = commands[key]
+            tasks.append(command_thread(key, sleep_period, command, output_type))
+        for task in tasks:
+            tg.create_task(task)
+
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
--- src/flask_server.py
+++ src/flask_server.py
@@ -1,59 +1,78 @@
 """
 Server that runs in the docker container
+It serves the pre-built vue frontend
 
-This server should call the blackbox processes and receive requests from the audio client
-NOTE: technically, the "client" application "serves" raw audio to this application
+NOTE: technically, the "client" application "serves" raw audio/data to this application
 
 
 Technical:
 audio is posted to ip/audio_in
-video should be available at ip/video/id
+video should be available at ip/output and ip/output/stream
 
-A Vue site should be the only thing that has direct communication with this application
-it should not be available outside of the docker container (except MAYBE the video stream)
-although for now it is available because the Vue site doesnt exist and I needed to test
-the audio input
 
 might want to open a second port for the audio if we are *slow* because of the constant audio requests
 - this is not a concern at the moment
 
 Server: Flask
+
+Tasks:
+[TODO] support caching for general data
+[TODO] handle general data
+[TODO] make general data accessable to frontend via api calls for specific keys
+[TODO] add route to send available keys to frontend
+[TODO] clean up audio handling
+[TODO] deal with keep-alive connections
+
 """
 from flask import Flask, render_template, request, jsonify
+from werkzeug.serving import WSGIRequestHandler
 import numpy as np
+import logging
 
 
 flask_app = Flask(__name__, template_folder='.')
+flask_app.logger.setLevel(logging.DEBUG)
 
 audio_str = ""
-audio_source = ""
 audio_raw_max = 0
 AUDIO_SAVED_CHUNKS = 5
 audio_last = []
 audio_max_last = audio_raw_max
 # TODO: find a good way to store many past chunks (preferably both push and pop are o(1))
 audio_chunk = []
-all_audio = {}
+# TODO: gracefully handle client settings
+change_settings = False
+client_audio_settings = dict()
+
+# general data
+general_data = dict()
+
+
 @flask_app.route("/")
 def main_page():
     return render_template('index.html')
 
-@flask_app.route("/audio_source", methods = ['GET', 'POST'])
-def get_audio_source():
+@flask_app.route("/audio_settings", methods = ['GET', 'POST'])
+def get_audio_settings():
     """
     This might end up being obsolete, but adding the header for now
     """
-    global all_audio
-    global audio_source
+    global client_audio_settings
+    global change_settings
     if request.method == "POST":
         data = request.form
-        if "mics" in data:
-            all_audio = data["mics"]
-        if "source" in data:
-            audio_source = data["source"]
-        return jsonify({"source": audio_source, "mics": all_audio})
+        response = {}
+        flask_app.logger.info(f"received {data}")
+        if "settings" in data:
+            response['message'] = f"Updated: {','.join(token for token in data['settings'])}"
+            for token in data['settings']:
+                client_audio_settings[token] = data[token]
+            change_settings = True
+        else:
+            response['message'] = "Error: settings must be a dict in a 'settings' key"
+        return jsonify(response)
     else:
-        return jsonify({"source": audio_source, "mics": all_audio})
+        return jsonify({"settings": client_audio_settings})
 
 
 @flask_app.route("/audio_in", methods=['GET', 'POST'])
@@ -78,6 +97,7 @@ def audio_in():
     global audio_last
     global audio_max_last
     global AUDIO_SAVED_CHUNKS
+    global change_settings
     if request.method == 'POST':
         data = request.json
         audio_chunk = np.array(data['data']).reshape(-1)
@@ -86,6 +106,7 @@ def audio_in():
         audio_raw_max = rpeak
         # NOTE: all of this computation is better done in a celery task, but since those arent set up yet,
         # doing basic analysis here
+        # NOTE: celery tasks may never be implemented because we no longer have a need for them (no librosa)
         audio_last.append(rpeak)
         if (len(audio_last) > AUDIO_SAVED_CHUNKS):
             audio_last.pop(0)
@@ -94,19 +115,49 @@ def audio_in():
         bars = "#" * int(50 * ravg)
         mbars = "-" * int((50 * rpeak) - (50 * ravg))
         audio_str = bars + mbars
-
         response = {"bars": audio_str}
-        if "source" in data:
-            if data["source"] != audio_source:
-                print(f"{audio_source} != {data['source']}")
-                response["source"] = audio_source
-        response = jsonify(response)
-        return response
+
+        if change_settings:
+            response['setting_change'] = change_settings
+            change_settings = False
+        return jsonify(response)
     else:
         response = jsonify({"bars": audio_str, "peak": audio_max_last, "source": audio_source})
         # TODO: the actual CORS policy
         response.headers.add("Access-Control-Allow-Origin", "*")
         return response
+
+@flask_app.route("/general_in", methods=['POST'])
+def general_in():
+    """
+    Recieve misc information of the form
+    { 
+    key: data,
+    type: type(data)
+    }
+
+    At the moment, only int is supported as a data type and
+    all data is assumed to be a range between 0 and 100
+    """
+    global general_data
+    assert request.method == 'POST', "the route /general_in only supports POSTs"
+    data = request.json
+    assert 'type' in data, "request to /general_in did not specify the data type"
+    for key in data:
+        if key == 'type':
+            continue
+        flask_app.logger.info(f"Updating general data: {key}: {data[key]}")
+        general_data[key] = data[key]
+    response = {"message": f"received data for {key in data if key != 'type' else ''}"}
+    return jsonify(response)
+
+@flask_app.route("/general_keys", methods=['GET'])
+def general_keys():
+    """
+    Return a list of all known general_data keys
+    """
+    response = {"keys": [key for key in general_data]}
+    return jsonify(response)
     
 
 @flask_app.route("/fft_audio", methods=['GET'])
@@ -131,9 +182,8 @@ def fft_audio():
 def output_page():
     """
     Display just the threejs scene.
-    This is how other programs get our output
+    This is how other programs get our output (as html)
     """
-    #TODO: tell the vue app to build a MPA
     return render_template("outputscene/outputscene.html")
 
 @flask_app.route("/output/stream", methods=['GET'])
@@ -153,4 +203,5 @@ def output_stream():
     
 
 if __name__ == "__main__":
+    WSGIRequestHandler.protocol_version = "HTTP/1.1"
     flask_app.run()
